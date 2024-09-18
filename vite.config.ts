6a2a3c21ef5748fc888392d5163412c6222d2e39
danil <ap.fomin203@gmail.com>
2024-09-18 19:03:38 +0300

    create pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,51 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
+import react from '@vitejs/plugin-react-swc';
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'IT-Cube: Расписание Занятий',
+        short_name: 'Расписание IT-Cube',
+        description: 'IT-Cube: Расписание Занятий — это удобное приложение, разработанное для родителей и учеников образовательного пространства IT-Cube. С его помощью вы сможете эффективно планировать учебный процесс и следить за расписанием занятий.',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#141924',
+        theme_color: '#141924',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/pwa-maskable-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/pwa-maskable-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     open: '/',
   },
-})
+});
